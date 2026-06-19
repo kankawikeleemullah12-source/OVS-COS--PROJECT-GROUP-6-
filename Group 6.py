915aac2ba919110a6c83990f0368d4e99b20c7ad
MMDESIGNS447 <bholarji32@gmail.com>
2026-06-19 18:19:47 +0100

    Update Group 6.py

--- Group 6.py
+++ Group 6.py
@@ -1,1 +1,11 @@
+import csv
 
+def run_dictionary_pipeline():
+    # Initialize a dynamic dictionary mapper for the 5 courses
+    metrics = {
+        "CHM102": 0,
+        "CHM108": 0,
+        "BIO102": 0,
+        "BIO108": 0,
+        "COS102": 0
+    }
